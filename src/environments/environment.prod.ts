da404c67fa3a6d38a2d2a1ba64a1eb00767abf81
jlabaj <labaj.jakub@gmail.com>
2023-02-08 10:54:22 +0100

    3 files modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: null, 
+  baseUrl: null,
   tokenUrl: null,
-  version: require('../../package.json').version,
   clientRoot: 'http://localhost:4200',
-  isMobile: false
-}
+  isMobile: false,
+};
