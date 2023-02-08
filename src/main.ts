242f365553a78678dd235036c7a5cf593932a118
jlabaj <labaj.jakub@gmail.com>
2023-02-08 11:33:45 +0100

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,30 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-export const getBaseUrl = () => {
-  return document.getElementsByTagName('base')[0].href;
-};
+// export const getBaseUrl = () => {
+//   return document.getElementsByTagName('base')[0].href;
+// };
 
 // const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
 // if (environment.production) {
 //   enableProdMode();
 // }
-const init = () => {
-  platformBrowserDynamic().bootstrapModule(AppModule, {
-    ngZoneEventCoalescing: true,
-  });
-  // .then((ref) => {
-  //   // Ensure Angular destroys itself on hot reloads.
-  //   if (window['ngRef']) {
-  //     window['ngRef'].destroy();
-  //   }
-  //   window['ngRef'] = ref;
+platformBrowserDynamic().bootstrapModule(AppModule);
+// const init = () => {
+//   platformBrowserDynamic().bootstrapModule(AppModule, {
+//     ngZoneEventCoalescing: true,
+//   });
+//   // .then((ref) => {
+//   //   // Ensure Angular destroys itself on hot reloads.
+//   //   if (window['ngRef']) {
+//   //     window['ngRef'].destroy();
+//   //   }
+//   //   window['ngRef'] = ref;
 
-  //   // Otherwise, log the boot error
-  // })
-  // .catch((err) => console.error(err));
-};
+//   //   // Otherwise, log the boot error
+//   // })
+//   // .catch((err) => console.error(err));
+// };
 
-init();
+// init();
